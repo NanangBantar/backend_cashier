75e97e1fb349b48c05b5402dfa8a1d887d038d26
NanangBantar <bantartrisna@gmail.com>
2021-12-22 14:33:39 +0700

    make login and create user route and functionality

--- routes/user/login_user.js
+++ routes/user/login_user.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
+dotenv.config();
 const User = require("../../models/User");
 
 router.post(
@@ -13,9 +16,43 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-
     try {
-    } catch (error) {}
+      const { username, password } = req.body;
+      const user = await User.findOne({
+        username,
+      });
+      if (user) {
+        const isMatch = await bcryptjs.compare(password, user.passwordText);
+        if (isMatch) {
+          const payload = {
+            id: user.id,
+            username,
+          };
+          const token = jwt.sign(payload, process.env.ACCESS_TOKEN);
+          return res.json({
+            token,
+            status: "success",
+            msg: "Successfully logged in..!",
+          });
+        }
+        return res.json({
+          status: "failed",
+          msg: "Wrong password or username..!",
+        });
+      }
+      return res.json({
+        status: "failed",
+        msg: "Account doesnt exist..!",
+      });
+    } catch (error) {
+      return res.json({
+        errors: [
+          {
+            msg: "Server Error",
+          },
+        ],
+      });
+    }
   }
 );
 
